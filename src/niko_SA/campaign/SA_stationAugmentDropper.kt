7b10463415112ba8279ca192aa5ddf7484680b71
nikothedude <simon.prouty@gmail.com>
2025-05-31 14:10:08 -0400

    le bugfix

--- src/niko_SA/campaign/SA_stationAugmentDropper.kt
+++ src/niko_SA/campaign/SA_stationAugmentDropper.kt
@@ -1,16 +1,12 @@
-package niko_SA
+package niko_SA.campaign
 
 import com.fs.starfarer.api.campaign.BaseCampaignEventListener
 import com.fs.starfarer.api.campaign.CargoAPI
 import com.fs.starfarer.api.campaign.FleetEncounterContextPlugin
 import com.fs.starfarer.api.campaign.SpecialItemData
-import data.scripts.campaign.magnetar.niko_MPC_omegaWeaponPurger
-import data.utilities.niko_MPC_battleUtils.getStationFleet
-import data.utilities.niko_MPC_ids
 import niko_SA.MarketUtils.getStationAugments
 import niko_SA.SA_fleetUtils.isStationFleet
 import niko_SA.scripts.stationMarketTracker
-import niko_SA.specialItems.SA_augmentBlueprintPlugin
 import org.lazywizard.lazylib.MathUtils
 
 class SA_stationAugmentDropper: BaseCampaignEventListener(false) {
@@ -23,11 +19,11 @@ class SA_stationAugmentDropper: BaseCampaignEventListener(false) {
         //val stationFleet = battle.getStationFleet() ?: return
         val stationFleet = battle.snapshotBothSides.firstOrNull { it.isStationFleet() } ?: return
         if (!(stationFleet.isEmpty || stationFleet.isDespawning || stationFleet.isExpired)) return // destroyed, TODO: inaccurate
-        val stationMarket = stationMarketTracker.getInstance().getMarketOfFleet(stationFleet) ?: return
+        val stationMarket = stationMarketTracker.Companion.getInstance().getMarketOfFleet(stationFleet) ?: return
         val augments = stationMarket.getStationAugments()
 
         for (augment in augments) {
-            val dropChance = (augment.getCombatDropChance() * 0.01f)
+            val dropChance = (augment.getCombatDropChance())
             if ((MathUtils.getRandom().nextFloat() * 100f) >= dropChance) continue
 
             loot.addSpecial(SpecialItemData("SA_augmentBlueprint", augment.id), 1f)
